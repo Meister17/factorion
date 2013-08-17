549be4554cba8ac1747eba889cf70d65f52487c5
Meister17 <mnokel@gmail.com>
2013-08-18 00:48:26 +0400

    Switch to russian comments

--- factorion.js
+++ factorion.js
@@ -1,13 +1,12 @@
 /**
-  * Finds all possible factorions - numbers equal to the sum of factorials of
-  * their digits
-  * @return {array} Array containing all possible factorions
+  * Находит все факторионы, т.е. числа, равные сумме факториалов своих цифр
+  * @return {Number}[] Массив всех найденных факторионов
   */
 function findFactorions() {
 
   /**
-    * Calculates factorials of all digits
-    * @return {array} Array containing factorials of all digits
+    * Вспомогательная функция. Вычисляет факториалы всех цифр.
+    * @return {Number}[] Массив факториалов всех цифр
     */
   function calculateDigitFactorials() {
     var factorials = [1];
@@ -20,10 +19,12 @@ function findFactorions() {
   }
 
   /**
-    * Finds the power of 10 that is the upper bound of factorions. Uses the
-    * following idea: if the number has d digits, then maximum sum of factorials
-    * of its digits equals to d * 9! So the number can not be greater than d * 9!
-    * @return the upper bound of factorions
+    * Находит такую степень десяти, что число в этой степени является верхней
+    * границей факторионов. Используется следующая идея: если число состоит из
+    * d цифр, то максимальная сумма факториалов цифр такого числа равна d * 9!
+    * Поэтому число не может быть больше этой суммы.
+    * @return {Number} такая степень десяти, что число в этой степени является
+    * верхней границей факторионов
     */
   function CalculateUpperBound(factorials) {
     var number = 2;
@@ -37,14 +38,14 @@ function findFactorions() {
   }
 
   /**
-    * Returns array of digits that will be used for faster updates of checking
-    * number. To find factorions we should iterate through all numbers from 0 to
-    * upper bound, and calculate sum of factorials of their digits. So to speed
-    * this process up we will use array of digits that represents the number
-    * under consideration.
-    * @param {number} no_digit constant that represents no digit
-    * @param {number} upper_bound upper bound of factorions
-    * @return {array} Array containing digits
+    * Возвращает массив, заполненный переданной константой no_digit и состоящий
+    * из upper_bound элементов (в последнем элементе 0). Такой массив будет
+    * использоваться для быстрого инкеремента числа и пересчета суммы
+    * факториалов его цифр.
+    * @param {Number} no_digit константа, означающая отсутствие цифры
+    * @param {Number} upper_bound верхняя граница факторионов
+    * @return {Number}[] массив, заполненной константой no_digit и состоящий из
+    * upper_bound элементов (в последнем элементе 0).
     */
   function InitializeDigits(no_digit, upper_bound) {
     var digits = [];
@@ -56,14 +57,14 @@ function findFactorions() {
   }
 
   /**
-    * Performs increment on the given number and updates the sum of factorials
-    * of its digits. In fact, we add 1 to the last element of the array with
-    * digits, and, if necessary, updates other elements.
-    * @param {number} no_digit constant representing no digit
-    * @param {number} sum current sum of factorials of digits
-    * @param {array} digits array containing digits representing the number
-    * @param {factorials} factorials array containing factorials of all digits
-    * @return {number} updated sum of factorials of the next number
+    * Осуществляет инкремент переданного числа и пересчет суммы факториалов его
+    * цифр. Фактически, добавляется 1 к последнему элементу массива, и затем,
+    * при необходимости, обновляются и другие элементы.
+    * @param {Number} no_digit константа, означающая отсутствие цифры
+    * @param {Number} sum текущая сумма факториалов цифр переданного числа
+    * @param {Number}[] digits массив, содержащий цифры переданного числа
+    * @param {Number}[] factorials массив, содержащий факториалы всех цифр
+    * @return {Number} обновлённая сумма факториалов цифр следующего числа
     */
   function IncrementNumber(no_digit, sum, digits, factorials) {
     for (var index = digits.length - 1; index >= 0; --index) {
